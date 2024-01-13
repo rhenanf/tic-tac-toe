dff964609088ab046eec7b20add942f50987c15b
rhfolco <rhenan.folco@argenti.com.au>
2024-01-13 18:18:17 +1100

    add ability to insert/change players names

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,16 @@
+import Player from './components/Player';
 
 function App() {
-  
-
-  return (
-    <main>
-      <div id="game-container">
-        <ol id="players">
-          <li>
-          <span className="player-name">Player 1</span>
-          <span className="player-symbol">X</span>
-          </li>
-          <li>
-          <span className="player-name">Player 2</span>
-          <span className="player-symbol">O</span>
-          </li>
-        </ol>
-      </div>
-    </main>
-  )
+	return (
+		<main>
+			<div id="game-container">
+				<ol id="players">
+          <Player initialName="Player 1" symbol="X"/>
+          <Player initialName="Player 2" symbol="O"/>
+				</ol>
+			</div>
+		</main>
+	);
 }
 
-export default App
+export default App;
